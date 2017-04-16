6e4122a6da25cdb296d8b2af7030c26d2b8f4257
Marina-chan <marina.k.chan@gmail.com>
2017-04-16 17:10:01 +0300

    Fixed some things

--- README.md
+++ README.md
@@ -1,16 +1,26 @@
 # telegram_welcomer_bot
 
-This bot will welcome anyone who will join group or supergroup and will store answers from joined user if needed.
+This bot will welcome anyone who will join supergroup and will store answers from joined user if needed.
 
 How to install and run bot:
 
 > Make sure that you use Python **3.6** or later
 
-Do `git clone` or download zip and unpack it, then open console and do
+Do `git clone` or download zip and unpack it, then open console and do:
 
 `python -m pip install -r requirements.txt`
 
-After that you need to write in bot_token (you can get it from **@botfather**) and your bot username and then you can run your bot via `python welcome.py`
+Create `admins.cfg` file and add ID's of admins in this file, each admin id in new line.
+
+After that you need to create database file named `answers.db` via sqlite3(in folder in which you unpacked/cloned repository):
+
+```sql lite
+>sqlite3
+sqlite3>.open --new answers.db
+sqlite3>CREATE TABLE user_answers (id int, user_message text);
+```
+
+And then you need to write in your bot token (you can get it from **@botfather**) and your bot username. Finally you can run your bot via `python welcome.py`
 
 
 
--- welcomer.py
+++ welcomer.py
@@ -4,88 +4,91 @@ import asyncio
 import logging
 import sqlite3
 from datetime import datetime
+from itertools import chain
 from pprint import pprint
 
-import aiohttp
 import telepot
 import telepot.aio
 
 import config
 
+
 logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s')
 logger = logging.getLogger(config.bot_username)
 bot = telepot.aio.Bot(config.bot_token)
 
-welcome_message = "Привет!"
-message_count = 0
-timeout = False
+user_ans_db = sqlite3.connect("answers.db")
+user_ans_curr = user_ans_db.cursor()
+
+admins_list = config.load_admins()
+got_user_response = list(chain.from_iterable(user_ans_curr.execute("SELECT id FROM user_answers")))
 
 
-async def switch_welcome_message():
-    global welcome_message
-    current_hour = datetime.now().hour()
-    if 0 > current_hour > 6:
-        welcome_message = "Доброй ночи, неспящий человек!"
-    elif 6 > current_hour > 10:
-        welcome_message = "Доброго утра!"
-    elif 10 > current_hour > 17:
-        welcome_message = "Доброго дня!"
-    elif 17 > current_hour >= 23:
-        welcome_message = "Доброго вечера!"
-    await asyncio.sleep(60 * 60)
+def switch_welcome_message():
+    current_hour = datetime.now().hour
+    if 0 >= current_hour <= 6:
+        return "Доброй ночи, неспящий человек"
+    elif 6 > current_hour <= 10:
+        return "Доброго утра"
+    elif 10 > current_hour <= 17:
+        return "Доброго дня"
+    elif 17 > current_hour <= 23:
+        return "Доброго вечера"
 
 
-async def set_timeout():
-    global timeout
-    await asyncio.sleep(60 * 15)
-    timeout = False
-
-
-async def welcome_user(username, msg_id):
-    global message_count, timeout
-    if not timeout:
-        while message_count < 7:
-            await asyncio.sleep(0.1)
-        if message_count >= 7:
-            await bot.sendMessage(chat_id=1,  # Your chat id
-                                  text=f"{welcome_message}, {username}! "
-                                       f"Расскажи немного о себе в реплае на это сообщение! "
-                                       f"Что умеете в сфере I.T.? Чего ждете от чата?",
-                                  reply_to_message_id=msg_id)
-            message_count = 0
-    else:
-        timeout = True
-        await set_timeout()
-        await bot.sendMessage(chat_id=1,  # Your chat id
-                              text=f"{welcome_message}, {username}! Расскажи немного о себе в реплае на это сообщение! "
-                                   f"Что умеете в сфере I.T.? Чего ждете от чата?",
-                              reply_to_message_id=msg_id)
+async def welcome_user(username, msg_id, chat_id):
+    await bot.sendMessage(chat_id=chat_id,
+                          text=f"{switch_welcome_message()}, {username}! "
+                               f"Расскажи немного о себе в реплае на это сообщение! "
+                               f"Что умеете в сфере I.T.? Чего ждете от чата?",
+                          reply_to_message_id=msg_id)
 
 
 async def handle(msg):
-    global message_count
     content_type, chat_type, chat_id = telepot.glance(msg)
-    pprint(msg)
-    if 'new_chat_member' in msg:
+    if chat_type == 'supergroup' and msg['from']['id'] in admins_list:
+        if 'reply_to_message' in msg:
+            await bot.sendMessage(chat_id=chat_id,
+                                  text=f"User ID: {msg['reply_to_message']['from']['id']}",
+                                  reply_to_message_id=msg['message_id'])
+    if 'new_chat_member' in msg and chat_type == 'supergroup':
+        logger.info(f"Got new chat member {msg['new_chat_member']['first_name']}")
         if 'username' in msg['new_chat_member']:
             await welcome_user("@" + msg['new_chat_member']['username'],
-                               msg['message_id'])
+                               msg['message_id'], chat_id)
         elif 'last_name' in msg['new_chat_member']:
             await welcome_user(msg['new_chat_member']['first_name'] + " " + msg['new_chat_member']['last_name'],
-                               msg['message_id'])
+                               msg['message_id'], chat_id)
         else:
             await welcome_user(msg['new_chat_member']['first_name'],
-                               msg['message_id'])
-    else:
-        message_count += 1
-    print(f"Time: {datetime.now().strftime('%H:%M:%S')}\n"
-          f"Chat: {content_type} {chat_type} {chat_id}\n")
+                               msg['message_id'], chat_id)
+    elif 'reply_to_message' in msg:
+        if msg['reply_to_message']['from']['username'] == config.bot_username[1:]:
+            if msg['from']['id'] not in got_user_response:
+                logger.info(f"Got response from user: {msg['from']['first_name']}, User ID: {msg['from']['id']}")
+                user_ans_curr.execute("INSERT INTO user_answers (id, user_message) VALUES (?, ?)",
+                                      (msg['from']['id'], msg['text']))
+                user_ans_db.commit()
+    elif chat_id in admins_list:
+        if 'forward_from' in msg:
+            if msg['forward_from']['id'] not in got_user_response:
+                user_ans_curr.execute("INSERT INTO user_answers (id, user_message) VALUES (?, ?)",
+                                      (msg['forward_from']['id'], msg['forward_from']['text']))
+                user_ans_db.commit()
+                got_user_response.append(msg['forward_from']['id'])
+                await bot.sendMessage(chat_id=chat_id,
+                                      text="Ответ был успешно записан в базу данных")
+            else:
+                await bot.sendMessage(chat_id=chat_id,
+                                      text="Ответ уже есть в базе данных")
+    logger.info(f"Time: {datetime.now().strftime('%H:%M:%S')}\n"
+                f"Chat: {content_type} {chat_type} {chat_id}")
+    pprint(msg)
 
 
 def main():
     loop = asyncio.get_event_loop()
 
-    loop.create_task(switch_welcome_message())
     loop.create_task(bot.message_loop(handle))
 
     loop.run_forever()
