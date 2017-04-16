0e37f907a0d3fdb683046190c1b57a2a29460c81
Marina-chan <marina.k.chan@gmail.com>
2017-04-16 23:03:06 +0300

    Small fixes with aio module

--- welcomer.py
+++ welcomer.py
@@ -12,22 +12,31 @@ import telepot.aio
 
 import config
 
-
 logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s')
 logger = logging.getLogger(config.bot_username)
 bot = telepot.aio.Bot(config.bot_token)
+loop = asyncio.get_event_loop()
 
 user_ans_db = sqlite3.connect("answers.db")
 user_ans_curr = user_ans_db.cursor()
 
 admins_list = config.load_admins()
 got_user_response = list(chain.from_iterable(user_ans_curr.execute("SELECT id FROM user_answers")))
+users = asyncio.Queue()
+
+"""
+{<Task pending coro=<handle() running at main.py:75>>, 
+<Task pending coro=<Bot.message_loop() running at \bots\lib\site-packages\telepot\aio\__init__.py:580> 
+wait_for=<Future pending cb=[<TaskWakeupMethWrapper object at 0x02586770>()]>>,
+<Task pending coro=<welcome_user() running at main.py:42>>}
+"""
 
 
 def switch_welcome_message():
     current_hour = datetime.now().hour
+    print(datetime().now().hour)
     if 0 >= current_hour <= 6:
-        return "Доброй ночи, неспящий человек"
+        return "Доброй ночи, неспящий(ие)"
     elif 6 > current_hour <= 10:
         return "Доброго утра,"
     elif 10 > current_hour <= 17:
@@ -36,15 +45,28 @@ def switch_welcome_message():
         return "Доброго вечера,"
 
 
-async def welcome_user(username, msg_id, chat_id):
-    await bot.sendMessage(chat_id=chat_id,
-                          text=f"{switch_welcome_message()} {username}! "
-                               f"Расскажи немного о себе в реплае на это сообщение! "
-                               f"Что умеете в сфере I.T.? Чего ждете от чата?",
-                          reply_to_message_id=msg_id)
+async def welcome_user(msg_id, chat_id):
+    global users
+    usernames = []
+    while not users.empty():
+        usernames.append(await users.get())
+        await asyncio.sleep(20)
+    if len(usernames) == 1:
+        await bot.sendMessage(chat_id=chat_id,
+                              text=f"{switch_welcome_message()} {usernames[0]}! "
+                                   f"Расскажи немного о себе в реплае на это сообщение! "
+                                   f"Что умеете в сфере I.T.? Чего ждете от чата?",
+                              reply_to_message_id=msg_id)
+    elif len(usernames) > 1:
+        welcome_users = ', '.join(usernames).strip()
+        await bot.sendMessage(chat_id=chat_id,
+                              text=f"{switch_welcome_message()} {welcome_users}! "
+                                   f"Расскажите немного о себе в реплае на это сообщение! "
+                                   f"Что умеете в сфере I.T.? Чего ждёте от чата?")
 
 
 async def handle(msg):
+    global users
     content_type, chat_type, chat_id = telepot.glance(msg)
     if chat_type == 'supergroup' and msg['from']['id'] in admins_list:
         if 'reply_to_message' in msg:
@@ -54,14 +76,25 @@ async def handle(msg):
     if 'new_chat_member' in msg and chat_type == 'supergroup':
         print(f"Got new chat member {msg['new_chat_member']['first_name']}")
         if 'username' in msg['new_chat_member']:
-            await welcome_user("@" + msg['new_chat_member']['username'],
-                               msg['message_id'], chat_id)
+            await users.put("@" + msg['new_chat_member']['username'])
+
+            # TODO: search for a way to track down running coroutine
+            # Example:
+            # if welcome_user in asyncio.Task.all_tasks():
+            #     pass
+            # else:
+            #     loop.create_task(welcome_user(*args))
+
+            loop.create_task(welcome_user(msg['message_id'], chat_id))
+            print(asyncio.Task.all_tasks())
         elif 'last_name' in msg['new_chat_member']:
-            await welcome_user(msg['new_chat_member']['first_name'] + " " + msg['new_chat_member']['last_name'],
-                               msg['message_id'], chat_id)
+            await users.put(msg['new_chat_member']['first_name'] + " " + msg['new_chat_member']['last_name'])
+            loop.create_task(welcome_user(msg['message_id'], chat_id))
+            print(asyncio.Task.all_tasks())
         else:
-            await welcome_user(msg['new_chat_member']['first_name'],
-                               msg['message_id'], chat_id)
+            await users.put(msg['new_chat_member']['first_name'])
+            loop.create_task(welcome_user(msg['message_id'], chat_id))
+            print(asyncio.Task.all_tasks())
     elif 'reply_to_message' in msg:
         if msg['reply_to_message']['from']['username'] == config.bot_username[1:]:
             if msg['from']['id'] not in got_user_response:
@@ -87,8 +120,6 @@ async def handle(msg):
 
 
 def main():
-    loop = asyncio.get_event_loop()
-
     loop.create_task(bot.message_loop(handle))
 
     loop.run_forever()
