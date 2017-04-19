b21f24a99b4e102a67552d5eca2d73d7d9329c15
Marina-chan <marina.k.chan@gmail.com>
2017-04-19 15:51:44 +0300

    Logging

--- welcomer.py
+++ welcomer.py
@@ -5,7 +5,7 @@ import logging
 import sqlite3
 from datetime import datetime
 from itertools import chain
-from pprint import pprint
+from json import dumps
 
 import telepot
 import telepot.aio
@@ -13,7 +13,7 @@ import telepot.aio
 import config
 
 
-logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s')
+logging.basicConfig(format='[%(asctime)s] %(levelname)s: %(message)s', level=logging.INFO)
 logger = logging.getLogger(config.bot_username)
 bot = telepot.aio.Bot(config.bot_token)
 loop = asyncio.get_event_loop()
@@ -70,29 +70,29 @@ async def handle(msg):
                                           text=f"User ID: {msg['reply_to_message']['from']['id']}",
                                           reply_to_message_id=msg['message_id'])
     if 'new_chat_member' in msg and chat_type == 'supergroup':
-        print(f"Got new chat member {msg['new_chat_member']['first_name']}")
+        logger.info(f"Got new chat member {msg['new_chat_member']['first_name']}")
         if 'username' in msg['new_chat_member']:
             await users.put("@" + msg['new_chat_member']['username'])
             if not chat_semaphores[chat_id]:
                 loop.create_task(welcome_user(msg['message_id'], chat_id))
                 chat_semaphores[chat_id] = True
-                print("Started coroutine")
+                logger.info("Started coroutine")
         elif 'last_name' in msg['new_chat_member']:
             await users.put(msg['new_chat_member']['first_name'] + " " + msg['new_chat_member']['last_name'])
             if not chat_semaphores[chat_id]:
                 loop.create_task(welcome_user(msg['message_id'], chat_id))
                 chat_semaphores[chat_id] = True
-                print("Started coroutine")
+                logger.info("Started coroutine")
         else:
             await users.put(msg['new_chat_member']['first_name'])
             if not chat_semaphores[chat_id]:
                 loop.create_task(welcome_user(msg['message_id'], chat_id))
                 chat_semaphores[chat_id] = True
-                print("Started coroutine")
+                logger.info("Started coroutine")
     elif 'reply_to_message' in msg:
         if msg['reply_to_message']['from']['username'] == config.bot_username[1:]:
             if msg['from']['id'] not in got_user_response:
-                print(f"Got response from user: {msg['from']['first_name']}, User ID: {msg['from']['id']}")
+                logger.info(f"Got response from user: {msg['from']['first_name']}, User ID: {msg['from']['id']}")
                 user_ans_curr.execute("INSERT INTO user_answers (id, user_message) VALUES (?, ?)",
                                       (msg['from']['id'], msg['text']))
                 user_ans_db.commit()
@@ -108,9 +108,8 @@ async def handle(msg):
             else:
                 await bot.sendMessage(chat_id=chat_id,
                                       text="Ответ уже есть в базе данных")
-    print(f"Time: {datetime.now().strftime('%H:%M:%S')}\n"
-          f"Chat: {content_type} {chat_type} {chat_id}")
-    pprint(msg)
+    logger.info(f"Chat: {content_type} {chat_type} {chat_id}\n"
+                f"{dumps(msg, indent=4)}")
 
 
 def main():
